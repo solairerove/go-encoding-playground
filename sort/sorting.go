1a7399cda50fe45feb156f86cbe65bece1306ac2
Mikita Kryvitski <solairerove@gmail.com>
2019-07-14 17:51:34 +0300

    feature: example of custom sort on struct

--- sort/sorting.go
+++ sort/sorting.go
@@ -5,6 +5,17 @@ import (
 	"sort"
 )
 
+type person struct {
+	first string
+	age   int
+}
+
+type sortByName []person
+
+func (a sortByName) Len() int           { return len(a) }
+func (a sortByName) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a sortByName) Less(i, j int) bool { return a[i].first < a[j].first }
+
 // Sorting tbd
 func Sorting() {
 	fmt.Println("\nSorting")
@@ -19,4 +30,34 @@ func Sorting() {
 	fmt.Println(xs)
 	sort.Strings(xs)
 	fmt.Println(xs)
+
+	fmt.Println("\nCustom Sorting")
+
+	p1 := person{
+		first: "Jack",
+		age:   33,
+	}
+
+	p2 := person{
+		first: "Tyler",
+		age:   33,
+	}
+
+	p3 := person{
+		first: "Marla",
+		age:   33,
+	}
+
+	p4 := person{
+		first: "Bob",
+		age:   43,
+	}
+
+	people := []person{
+		p1, p2, p3, p4,
+	}
+
+	fmt.Println(people)
+	sort.Sort(sortByName(people))
+	fmt.Println(people)
 }
